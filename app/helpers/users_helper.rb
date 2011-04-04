a5eb70ecb499a305c12b23c4e25b4abeef3658b9
alumnirails <alumnirails@gmail.com>
2011-04-05 01:02:21 +0530

    Done with user edit/update,index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,14 @@ def gravatar_for(user, options = { :size => 50 })
                                             :class => 'gravatar',
                                             :gravatar => options)
   end
+
+
+def gravatar_url_for(email, options = {})      
+url_for({ :gravatar_id => Digest::MD5.hexdigest(email), 
+          :host => 'www.gravatar.com',
+          :protocol => 'http://', 
+          :only_path => false, 
+          :controller => 'avatar.php'}.merge(options))  
+end
+
 end
